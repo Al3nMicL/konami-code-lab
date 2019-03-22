da97437ab3a7ef94d10237d01bb19ee893c3e5c9
Al3nMicL <https://twitter.com/Al3nMicL>
2019-03-22 19:26:33 -0400

    even easier solution using Mousetrap.js

--- konami_code.js
+++ konami_code.js
@@ -11,38 +11,12 @@ const codes = [
   "a"           // 65
 ];
 
-// store key code combo sequence to check
-// const combo = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-
-let keyPressed = '';
-let keyCombos = [];
-let pressCount = 0;
-
 function init() {
-  
-  // attach event listener to document
-  document.onkeydown = function(e) {
-
-  keyPressed = e.key;
-  keyCombos.push(keyPressed);
-  pressCount++;
-
-  // only run checks once valid sequence length is reached
-  if (pressCount >= codes.length) {  
-    
-    if (keyCombos.slice(-10).toString() == codes.toString()) {
-      alert("Combo was hit!");
-      keyCombos.length = 0; // reset array to start over
-    }
-
-  }
-  
-  // console.log(e.which); 
-  console.log(e.key); // sanity check
+  Mousetrap.bind('up up down down left right left right b a', function() { 
+    alert("Konami");
+  });  
 };
-
-  
-}
+ 
 
 // run the function
 init();
